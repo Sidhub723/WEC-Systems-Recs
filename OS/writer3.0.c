a097143b18fd24236578f7ccee38d351fe363382
Sidhub723 <sidbhat.211ee151@nitk.edu.in>
2022-11-02 21:11:51 +0530

    Comment cleanup

--- OS/writer3.0.c
+++ OS/writer3.0.c
@@ -10,27 +10,7 @@
 
 int main() 
 {
-    //void *shared_mem_addr;
-    //char inputs[100];
-    //int shmid;
-    //key_t keyy = ftok("/home/pingpong1/Desktop/VSC/fff.txt",'b');
-    //shmid = shmget((key_t)1234,BUF_SIZE,0666|IPC_CREAT);     //creating a segment and getting its id
-    //shared_mem_addr = shmat(shmid,NULL,0);      //returns the address space/adress of the segment where the segment gets attached to the prcess
-    //printf("ID if shared memory is %d \n",shmid);
-    //printf("Address of shared memory is %p \n",shared_mem_addr);
-    //int failedid = shmget((key_t)1234,BUF_SIZE,0666|IPC_EXCL|IPC_CREAT);
-    //printf("Failed id if %d",failedid);
-    //char *addr_cpy = (char *)shared_mem_addr;
-    //int printed = 0;
-    //memset(shared_mem_addr,1,BUF_SIZE);
-    //strcpy(addr_cpy,"$$$");                 //adds 3 dollar signs at the beginning of the memory segment to show that it is in use
-    //addr_cpy = addr_cpy + 3;                // and no other process can access it
-                                            // it is all under the assumption that this is the very writer which created the shared memory
-                                            // segment, there can be other writers which simply use this segment, NOT create it
-                                            // write a writer process which simply writer to an already created shared memeory segment
-                                            // or maybe, as a writer, check if a shared memeory segment exists, if not create it, if it does 
-                                            // them simply write to it.
-    //printf("The dollar signs are visible or not??? %s",(char *)shared_mem_addr);
+    
     void *shared_mem_addr;
     char inputs[100];
     int shmid;
